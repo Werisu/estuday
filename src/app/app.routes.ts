ceb22a70dadbc8e65ecc970310274976d5fb3278
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2026-01-12 17:08:34 -0300

    Remove NxWelcome component and update routing to include a lazy-loaded login component. Refactor app module imports to streamline the application structure.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'login',
+    loadComponent: () => import('@estuday/auth').then((m) => m.LoginComponent),
+  },
+];
